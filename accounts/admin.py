8c34e06723a190b27ec8f59e871948a31090cb35
AliJalili <pandaj200021@gmail.com>
2025-08-12 19:24:47 +0330

    register custom User model with custom admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from .forms import UserCreationForm, UserChangeForm
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username', 'email', 'phone_number', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('username', 'phone_number', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_admin', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'phone_number', 'email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('username', 'email', 'phone_number')
+    ordering = ('username',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
