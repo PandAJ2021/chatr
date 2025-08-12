2946a49d99be1aa965fed6518547775e3a20fafe
AliJalili <pandaj200021@gmail.com>
2025-08-12 19:20:49 +0330

    User custom model added.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(verbose_name="Email Address", unique=True, max_length=255)
+    join_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email', 'username']
+
+    def __str__(self):
+        return self.email
