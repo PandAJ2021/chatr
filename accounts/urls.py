67cb45645f08f2bf87ecaa02a2db2d646291baa3
AliJalili <pandaj200021@gmail.com>
2025-08-13 15:28:29 +0330

    register, login, logout and home endpoints added in urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'accounts'
-urlpatterns = []
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('register/', views.UserRegisterView.as_view(), name='register_user'),
+    path('login/', views.UserLoginView.as_view(), name='login_user'),
+    path('logout/', views.UserLogOutView.as_view(), name='logout_user'),
+]
