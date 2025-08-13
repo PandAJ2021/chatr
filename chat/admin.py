e95d4461f8d5c82e78bc53418ae8641043c51a44
AliJalili <pandaj200021@gmail.com>
2025-08-13 18:45:52 +0330

    register chat models in admin panel

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import ChatMessage, ChatRoom, ChatRoomMembership
 
-# Register your models here.
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room', 'timestamp', 'updated',)
+    list_filter = ('is_deleted',)
+    search_fields = ('user', 'room',)
+
+
+@admin.register(ChatRoom)
+class CharRoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'created_at',)
+    search_fields = ('name', 'author',)
+
+
+@admin.register(ChatRoomMembership)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room', 'joined_at',)
+    search_fields = ('user', 'room',)
