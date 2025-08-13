348997f35b20e03eef6c884a799bdf5094fce575
AliJalili <pandaj200021@gmail.com>
2025-08-13 15:24:43 +0330

    Add custom validator to phone_number field

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import UserManager
+from .validators import validate_phone_number
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=11, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True, validators=[validate_phone_number])
     email = models.EmailField(verbose_name="Email Address", unique=True, max_length=255)
     join_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
