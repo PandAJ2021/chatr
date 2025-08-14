ea3bdc6b51c788dad81b13aa23d03584ac3f20dd
AliJalili <pandaj200021@gmail.com>
2025-08-14 19:03:23 +0330

    add URL endpoints for chat room views

--- chat/urls.py
+++ chat/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'chat'
-urlpatterns = []
+urlpatterns = [
+    path('room/<str:room_name>/', views.chat_room, name='real_time_chat'), 
+    path('create-room/', views.CreateChatRoomView.as_view(), name='create_chat_room'),
+    path('room/leave/<int:membership_id>/', views.LeaveChatRoomView.as_view(), name='leave_room'),
+]
