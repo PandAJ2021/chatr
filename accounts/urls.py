ed26a136455e6253e09347e5531f0b4216be8645
AliJalili <pandaj200021@gmail.com>
2025-08-14 19:00:34 +0330

    UserProfile endpoints added in urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.home_view, name='home'),
     path('register/', views.UserRegisterView.as_view(), name='register_user'),
     path('login/', views.UserLoginView.as_view(), name='login_user'),
     path('logout/', views.UserLogOutView.as_view(), name='logout_user'),
+    path('profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path('profile/edit/', views.UserEditProfileView.as_view(), name='edit_profile'),
 ]
