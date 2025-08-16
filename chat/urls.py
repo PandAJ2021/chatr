ffd7fb9f668258e107543a2cb117e7f329b4d6a1
AliJalili <pandaj200021@gmail.com>
2025-08-16 17:31:29 +0330

    add URLs for listing and deleting chat rooms

--- chat/urls.py
+++ chat/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('room/<str:room_name>/', views.chat_room, name='real_time_chat'), 
     path('create-room/', views.CreateChatRoomView.as_view(), name='create_chat_room'),
     path('room/leave/<int:membership_id>/', views.LeaveChatRoomView.as_view(), name='leave_room'),
+    path('', views.ListChatRoomView.as_view(), name='all_rooms'), 
+    path('room/delete/<int:room_id>/', views.DeleteChatRoomView.as_view(), name='delete_room'),
 ]
