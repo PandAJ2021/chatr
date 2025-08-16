ad8ece2043579f8b04e2cebb4df0f7c729b54964
AliJalili <pandaj200021@gmail.com>
2025-08-16 17:29:33 +0330

    - ListChatRoomView to display all chat rooms - DeleteChatRoomView to allow authors to delete their own rooms

--- chat/views.py
+++ chat/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CreateChatForm
-from .models import ChatRoomMembership
+from .models import ChatRoomMembership, ChatRoom
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def chat_room(request, room_name):
     return render(request, 'chat/room.html', {'room_name': room_name})
 
@@ -32,4 +34,20 @@ class LeaveChatRoomView(LoginRequiredMixin, View):
         membership = get_object_or_404(ChatRoomMembership, pk=membership_id, user=request.user)
         membership.delete()
         messages.success(request, 'You left the group.', 'success')
+        return redirect('accounts:user_profile')
+
+
+class ListChatRoomView(LoginRequiredMixin, View):
+
+    def get(self, request):
+        rooms = ChatRoom.objects.all()
+        return render(request, 'chat/chat_rooms.html', {'rooms':rooms})
+
+
+class DeleteChatRoomView(LoginRequiredMixin, View):
+
+    def get(self, request, room_id):
+        room = get_object_or_404(ChatRoom, pk=room_id, author=request.user)
+        room.delete()
+        messages.success(request, 'You delete the group.', 'success')
         return redirect('accounts:user_profile')
